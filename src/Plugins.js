c2c96d2402231d8040343d67bb802f8fa7d9754b
Basit Mustafa <basit.mustafa@gmail.com>
2020-05-05 16:41:04 -0700

    Just a bit of LSD...
    Local Storage Data

--- src/Plugins.js
+++ src/Plugins.js
@@ -23,7 +23,7 @@ const puppeteer = require('puppeteer')
 module.exports.GoogleSocialLogin = async function GoogleSocialLogin(options = {}) {
   validateOptions(options)
 
-  const launchOptions = {headless: !!options.headless}
+  const launchOptions = { headless: !!options.headless }
 
   if (options.args && options.args.length) {
     launchOptions.args = options.args
@@ -32,10 +32,10 @@ module.exports.GoogleSocialLogin = async function GoogleSocialLogin(options = {}
   const browser = await puppeteer.launch(launchOptions)
   let page = await browser.newPage()
   let originalPageIndex = 1
-  await page.setViewport({width: 1280, height: 800})
+  await page.setViewport({ width: 1280, height: 800 })
 
   await page.goto(options.loginUrl)
-  await login({page, options})
+  await login({ page, options })
 
   // Switch to Popup Window
   if (options.isPopup) {
@@ -50,8 +50,8 @@ module.exports.GoogleSocialLogin = async function GoogleSocialLogin(options = {}
     page = pages[pages.length - 1]
   }
 
-  await typeUsername({page, options})
-  await typePassword({page, options})
+  await typeUsername({ page, options })
+  await typePassword({ page, options })
 
   // Switch back to Original Window
   if (options.isPopup) {
@@ -66,17 +66,18 @@ module.exports.GoogleSocialLogin = async function GoogleSocialLogin(options = {}
     await delay(options.cookieDelay)
   }
 
-  const cookies = await getCookies({page, options})
+  const cookies = await getCookies({ page, options });
+  const lsd = await getLocalStorageData({ page, options });
 
-  await finalizeSession({page, browser, options})
+  await finalizeSession({ page, browser, options })
 
   return {
-    cookies
+    cookies, lsd
   }
 }
 
 function delay(time) {
-  return new Promise(function(resolve) {
+  return new Promise(function (resolve) {
     setTimeout(resolve, time)
   })
 }
@@ -87,7 +88,7 @@ function validateOptions(options) {
   }
 }
 
-async function login({page, options} = {}) {
+async function login({ page, options } = {}) {
   if (options.preLoginSelector) {
     await page.waitForSelector(options.preLoginSelector)
     await page.click(options.preLoginSelector)
@@ -102,7 +103,7 @@ async function login({page, options} = {}) {
   await page.click(options.loginSelector)
 }
 
-async function typeUsername({page, options} = {}) {
+async function typeUsername({ page, options } = {}) {
   let buttonSelector = options.headless ? '#next' : '#identifierNext'
 
   await page.waitForSelector('input[type="email"]')
@@ -110,16 +111,16 @@ async function typeUsername({page, options} = {}) {
   await page.click(buttonSelector)
 }
 
-async function typePassword({page, options} = {}) {
+async function typePassword({ page, options } = {}) {
   let buttonSelector = options.headless ? '#signIn' : '#passwordNext'
 
-  await page.waitForSelector('input[type="password"]', {visible: true})
+  await page.waitForSelector('input[type="password"]', { visible: true })
   await page.type('input[type="password"]', options.password)
-  await page.waitForSelector(buttonSelector, {visible: true})
+  await page.waitForSelector(buttonSelector, { visible: true })
   await page.click(buttonSelector)
 }
 
-async function getCookies({page, options} = {}) {
+async function getCookies({ page, options } = {}) {
   await page.waitForSelector(options.postLoginSelector)
 
   const cookies = options.getAllBrowserCookies
@@ -133,11 +134,30 @@ async function getCookies({page, options} = {}) {
   return cookies
 }
 
+async function getLocalStorageData({ page, options } = {}) {
+  await page.waitForSelector(options.postLoginSelector)
+
+  const localStorageData = await page.evaluate(() => {
+    let json = {};
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      json[key] = localStorage.getItem(key);
+    }
+    return json;
+  });
+  if (options.logs) {
+    console.log(localStorageData)
+  }
+
+  return localStorageData
+}
+
+
 async function getCookiesForAllDomains(page) {
   const cookies = await page._client.send('Network.getAllCookies', {})
   return cookies.cookies
 }
 
-async function finalizeSession({page, browser, options} = {}) {
+async function finalizeSession({ page, browser, options } = {}) {
   await browser.close()
 }
