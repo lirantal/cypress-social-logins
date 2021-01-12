b26a01b452b4990dcd13d4707d5252cdb61c6548
InvisibleExo <rigatoniman@hotmail.com>
2021-01-11 20:54:10 -0800

    Create Plugins.d.ts

---
+++ src/Plugins.d.ts
@@ -0,0 +1,35 @@
+export declare function GoogleSocialLogin(options?: {}): Promise<{
+    cookies: any;
+    lsd: any;
+    ssd: any;
+}>;
+export declare function GitHubSocialLogin(options?: {}): Promise<{
+    cookies: any;
+    lsd: any;
+    ssd: any;
+}>;
+export declare function MicrosoftSocialLogin(options?: {}): Promise<{
+    cookies: any;
+    lsd: any;
+    ssd: any;
+}>;
+export declare function AmazonSocialLogin(options?: {}): Promise<{
+    cookies: any;
+    lsd: any;
+    ssd: any;
+}>;
+export declare function FacebookSocialLogin(options?: {}): Promise<{
+    cookies: any;
+    lsd: any;
+    ssd: any;
+}>;
+export declare function CustomizedLogin(options?: {}): Promise<{
+    cookies: any;
+    lsd: any;
+    ssd: any;
+}>;
+export declare function baseLoginConnect(typeUsername: any, typePassword: any, otpApp: any, authorizeApp: any, postLogin: any, options: any): Promise<{
+    cookies: any;
+    lsd: any;
+    ssd: any;
+}>;
